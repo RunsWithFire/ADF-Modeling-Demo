2e5f27b2c4a77dd1c00019a4786a1b43115fe116
MrBrent <Brent.hoch@gmail.com>
2017-05-07 18:27:40 -0500

    Aircraft number label and button

---
+++ Assets/Scripts/General/SessionData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Singleton Class
+public class SessionData : MonoBehaviour {
+
+	public static SessionData Instance;
+
+	public string aircraftNumber;
+
+	void Awake(){
+
+		if (Instance == null) {
+			Instance = this;
+		} else if(Instance != this){
+			Destroy (gameObject);
+		}
+
+		// don't destroy this object when a new Scene is loaded
+		DontDestroyOnLoad (gameObject);
+
+	}
+}
