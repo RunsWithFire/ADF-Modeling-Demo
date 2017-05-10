5b8bd79339676ded22163098fd93e2ac62f0362d
MrBrent <Brent.hoch@gmail.com>
2017-05-09 21:32:20 -0500

    Added animation to Damage Entry Screen/ fixed Menu

--- Assets/Scripts/EnterDamage.cs
+++ Assets/Scripts/EnterDamage.cs
@@ -5,17 +5,37 @@ using UnityEngine.UI;
 
 public class EnterDamage : MonoBehaviour {
 
-	private string NEW_DAMAGE_LABEL = "New Damage";
-	private string CLOSE_LABEL = "Close";
+	public string NEW_DAMAGE_LABEL = "New Damage";
+	public string CLOSE_LABEL = "Close";
 	public GameObject theCanvas;
+	private Animator slideInAnimation;
 
+	public void Start(){
+		Text[] ButtonsText = GetComponentsInChildren<UnityEngine.UI.Text>();
+		ButtonsText[0].text = NEW_DAMAGE_LABEL;
+	}
 
 	public void onClick(){
+		getAnimator (theCanvas);
 		Text[] ButtonsText = GetComponentsInChildren<UnityEngine.UI.Text>();
 		if (ButtonsText[0].text == CLOSE_LABEL) {
 			ButtonsText[0].text = NEW_DAMAGE_LABEL;
+			slideInAnimation.enabled = true;
+			slideInAnimation.Play("SlideDamageEntryCanvasOut");
+
 		} else {
 			ButtonsText[0].text = CLOSE_LABEL;
+			slideInAnimation.enabled = true;
+			slideInAnimation.Play("SlideDamageEntryCanvasIn");
 		}
+	}
+
+
+	void getAnimator(GameObject theCanv){
+		if (slideInAnimation == null) {
+			slideInAnimation = theCanvas.GetComponent<Animator>();
+		}
+
+
 	}
 }
