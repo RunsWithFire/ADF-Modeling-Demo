36be149315e914f3c230ecc96b37495ac0b7a590
Cole Carver <RunsWithFire@gmail.com>
2017-05-09 23:22:50 -0400

    Cleaning up DamageController

--- Assets/Scripts/damageController.cs
+++ Assets/Scripts/damageController.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class damageController : MonoBehaviour {
 
     public GameObject stateMachine;
     public GameObject damage;
+
+    private GameObject newDamage;
+    public Dropdown dropdown;
+
+    public Material scratchDamage;
+    public Material dentDamage;
+    public Material gashDamage;
+    public Material divotDamage;
+    public Material lightningDamage;
 
 	// Use this for initialization
 	void Start () {
@@ -14,31 +24,45 @@ public class damageController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
-        // If click on Damage ball.
-		if (Input.GetMouseButtonDown(0))
+        moveToViewDamage();
+        createDamage();
+        newDamageColorUpdate();
+	}
+
+    // If you click on a ball while in Aircraft Viewer mode.
+    public void moveToViewDamage()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            if (stateMachine.GetComponent<stateMachineController>().state.Equals("ViewAircraft"))
             {
-                if (hit.transform.name.Contains("Damage"))
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
                 {
-                    stateMachine.GetComponent<stateMachineController>().state = "ViewDamage";
+                    if (hit.transform.name.Contains("Damage"))
+                    {
+                        stateMachine.GetComponent<stateMachineController>().state = "ViewDamage";
+                    }
                 }
             }
         }
+    }
 
+    // If you are in the create damage menu and you click on the aircraft.
+    public void createDamage()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            if (stateMachine.GetComponent<stateMachineController>().state.Equals("CreateDamage")) {
+            if (stateMachine.GetComponent<stateMachineController>().state.Equals("CreateDamage"))
+            {
                 RaycastHit hit;
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 if (Physics.Raycast(ray, out hit))
                 {
                     if (hit.transform.name.Contains("B747"))
                     {
-                        GameObject newDamage = Instantiate(damage, hit.point, new Quaternion());
+                        newDamage = Instantiate(damage, hit.point, new Quaternion());
                         newDamage.transform.parent = GameObject.Find("B-747").transform;
 
                         stateMachine.GetComponent<stateMachineController>().state = "MoveDamage";
@@ -46,5 +70,32 @@ public class damageController : MonoBehaviour {
                 }
             }
         }
-	}
+    }
+
+    // Makes sure the new damage is the proper color.
+    public void newDamageColorUpdate()
+    {
+        if (stateMachine.GetComponent<stateMachineController>().state.Equals("MoveDamage"))
+        {
+            int dropdownValue =  dropdown.GetComponent<Dropdown>().value;
+            switch (dropdownValue)
+            {
+                case 0:
+                    newDamage.GetComponent<Renderer>().material = scratchDamage;
+                    break;
+                case 1:
+                    newDamage.GetComponent<Renderer>().material = dentDamage;
+                    break;
+                case 2:
+                    newDamage.GetComponent<Renderer>().material = gashDamage;
+                    break;
+                case 3:
+                    newDamage.GetComponent<Renderer>().material = divotDamage;
+                    break;
+                case 4:
+                    newDamage.GetComponent<Renderer>().material = lightningDamage;
+                    break;
+            }
+        }
+    }
 }
