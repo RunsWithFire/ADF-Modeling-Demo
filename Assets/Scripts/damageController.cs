83da27af52477c8590710c713645f1549fbf4549
Cole Carver <RunsWithFire@gmail.com>
2017-05-09 22:06:57 -0400

    Damage Object Created

---
+++ Assets/Scripts/damageController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageController : MonoBehaviour {
+
+    public GameObject stateMachine;
+
+    public string damageType;
+    public string height;
+    public string width;
+
+	// Use this for initialization
+	void Start () {
+        stateMachine = GameObject.Find("StateMachine");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name.Equals("Damage"))
+                {
+                    stateMachine.GetComponent<stateMachineController>().state = "ViewDamage";
+                }
+            }
+        }
+	}
+}
