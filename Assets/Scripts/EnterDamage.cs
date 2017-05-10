b3eaec36024026a79adbffe74a87fed0d731bbf2
Cole Carver <RunsWithFire@gmail.com>
2017-05-09 22:45:59 -0400

    Animator Fixed

--- Assets/Scripts/EnterDamage.cs
+++ Assets/Scripts/EnterDamage.cs
@@ -6,39 +6,18 @@ using UnityEngine.UI;
 public class EnterDamage : MonoBehaviour
 {
 
-<<<<<<< HEAD
     private string NEW_DAMAGE_LABEL = "New Damage";
     private string CLOSE_LABEL = "Close";
     public GameObject theCanvas;
     public GameObject stateMachine;
-=======
-	public string NEW_DAMAGE_LABEL = "New Damage";
-	public string CLOSE_LABEL = "Close";
-	public GameObject theCanvas;
 	private Animator slideInAnimation;
->>>>>>> refs/remotes/origin/Brent-ADF-Branch
+
 
 	public void Start(){
 		Text[] ButtonsText = GetComponentsInChildren<UnityEngine.UI.Text>();
 		ButtonsText[0].text = NEW_DAMAGE_LABEL;
 	}
 
-<<<<<<< HEAD
-    public void onClick()
-    {
-        Text[] ButtonsText = GetComponentsInChildren<UnityEngine.UI.Text>();
-        if (ButtonsText[0].text == CLOSE_LABEL)
-        {
-            ButtonsText[0].text = NEW_DAMAGE_LABEL;
-            stateMachine.GetComponent<stateMachineController>().state = "ViewAircraft";
-        }
-        else
-        {
-            ButtonsText[0].text = CLOSE_LABEL;
-            stateMachine.GetComponent<stateMachineController>().state = "CreateDamage";
-        }
-    }
-=======
 	public void onClick(){
 		getAnimator (theCanvas);
 		Text[] ButtonsText = GetComponentsInChildren<UnityEngine.UI.Text>();
@@ -46,12 +25,14 @@ public class EnterDamage : MonoBehaviour
 			ButtonsText[0].text = NEW_DAMAGE_LABEL;
 			slideInAnimation.enabled = true;
 			slideInAnimation.Play("SlideDamageEntryCanvasOut");
+            stateMachine.GetComponent<stateMachineController>().state = "ViewAircraft";
 
-		} else {
+        } else {
 			ButtonsText[0].text = CLOSE_LABEL;
 			slideInAnimation.enabled = true;
 			slideInAnimation.Play("SlideDamageEntryCanvasIn");
-		}
+            stateMachine.GetComponent<stateMachineController>().state = "CreateDamage";
+        }
 	}
 
 
@@ -62,5 +43,4 @@ public class EnterDamage : MonoBehaviour
 
 
 	}
->>>>>>> refs/remotes/origin/Brent-ADF-Branch
 }
